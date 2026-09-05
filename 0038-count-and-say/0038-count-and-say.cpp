239abcad964dfc7530b32a5e671e954d337303f4
Md Shahjad <mdshahjadms0786@gmail.com>
2026-09-05 13:09:13 +0530

    Time: 4 ms (45.45%), Space: 9.8 MB (80.92%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s = "1";
+
+        for (int k = 1; k < n; k++) {
+            string t = "";
+
+            for (int i = 0; i < s.size();) {
+                int j = i;
+
+                while (j < s.size() && s[j] == s[i])
+                    j++;
+
+                t += to_string(j - i);
+                t += s[i];
+
+                i = j;
+            }
+
+            s = t;
+        }
+
+        return s;
+    }
+};
