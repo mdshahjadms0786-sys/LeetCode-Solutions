4e5196bbecbad38aa09ae9952aa2d01a0cec3dae
Md Shahjad <mdshahjadms0786@gmail.com>
2026-09-09 07:19:13 +0530

    Time: 0 ms (100%), Space: 9 MB (31.03%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1);
+
+        dp[0] = 1;
+        dp[1] = (s[0] != '0');
+
+        for (int i=2; i <=n; i++) {
+            if (s[i - 1] != '0')
+                dp[i] += dp[i-1];
+
+            int num = stoi(s.substr(i-2,2));
+
+            if(num>=10 && num <=26)
+                dp[i] += dp[i-2];
+        }
+
+        return dp[n];
+        
+    }
+};
